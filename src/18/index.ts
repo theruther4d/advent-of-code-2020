3a6d2dc1c1e308e17fb81448c93578d0cc1a40f6
theruther4d <joshua.rutherford1@gmail.com>
2021-01-29 09:15:02 -0600

    Day 18 Part 2.

--- src/18/index.ts
+++ src/18/index.ts
@@ -1,64 +1,78 @@
-export function evaluate(expression: string): number {
-  if (numeric.test(expression)) {
-    const [, stringValue] = numeric.exec(expression);
-    return parseInt(stringValue);
-  }
+function evaluator(expression: string, parsers: Parser[]) {
+  parsers.forEach((parser) => {
+    while (parser.test(expression)) {
+      expression = expression.replace(parser.regex, (_, ...matches) => {
+        return String(
+          parser.transform((match) => evaluator(match, parsers), ...matches)
+        );
+      });
+    }
+  });
 
-  while (parser.test(expression)) {
-    expression = expression.replace(parser, (_, left, operator, right) => {
-      switch (operator) {
-        case "+":
-          return String(evaluate(left) + evaluate(right));
-        case "*":
-          return String(evaluate(left) * evaluate(right));
-        default:
-          throw new Error(`Invalid operator "${operator}"`);
-      }
-    });
-  }
-
-  return evaluate(expression);
+  return parseInt(expression);
 }
 
-export function evaluateV2(expression: string): number {
-  if (numeric.test(expression)) {
-    const [, stringValue] = numeric.exec(expression);
-    return parseInt(stringValue);
-  }
-
-  while (addParser.test(expression)) {
-    expression = expression.replace(addParser, (_, left, operator, right) => {
-      switch (operator) {
-        case "+":
-          return String(evaluate(left) + evaluate(right));
-        // case "*":
-        //   return String(evaluate(left) * evaluate(right));
-        default:
-          throw new Error(`Invalid operator in addParser "${operator}"`);
-      }
-    });
-  }
-
-  while (multiplyParser.test(expression)) {
-    expression = expression.replace(
-      multiplyParser,
-      (_, left, operator, right) => {
-        switch (operator) {
-          // case "+":
-          //   return String(evaluate(left) + evaluate(right));
-          case "*":
-            return String(evaluate(left) * evaluate(right));
-          default:
-            throw new Error(`Invalid operator in multiplyParser "${operator}"`);
-        }
-      }
-    );
-  }
-
-  return evaluate(expression);
+function add(evaluate: Evaluator, left: string, right: string) {
+  return evaluate(left) + evaluate(right);
 }
 
-const numeric = /^\(?(\d+)\)?$/;
-const parser = /(\d+|\([^\(\)]+\))\s([\+\*])\s(\d+|\([^\(\)]+\))/;
-const addParser = /(\d+|\([^\(\)]+\))\s([\+])\s(\d+|\([^\(\)]+\))/;
-const multiplyParser = /(\d+|\([^\(\)]+\))\s([\+])\s(\d+|\([^\(\)]+\))/;
+function multiply(evaluate: Evaluator, left: string, right: string) {
+  return evaluate(left) * evaluate(right);
+}
+
+class Parser {
+  constructor(
+    public regex: RegExp,
+    public transform: (evaluator: Evaluator, ...matches: string[]) => number
+  ) {}
+
+  test = (value: string) => this.regex.test(value);
+}
+
+const uselessParenthesisParser = new Parser(
+  /\((\d+)\)/,
+  (evaluate, soleNumber) => evaluate(soleNumber)
+);
+
+const parenthesisParser = new Parser(/\(([^\(\)]+)\)/, (parse, contents) =>
+  parse(contents)
+);
+
+const multiplicationParser = new Parser(/(\d+)\s\*\s(\d+)/, multiply);
+
+const additionParser = new Parser(/(\d+)\s\+\s(\d+)/, add);
+
+const additionOrMultiplicationParser = new Parser(
+  /(\d+)\s([\+\*])\s(\d+)/,
+  (evaluate, left, operator, right) => {
+    switch (operator) {
+      case "+":
+        return add(evaluate, left, right);
+      case "*":
+        return multiply(evaluate, left, right);
+      default:
+        throw new Error(
+          `Invalid operator in additionOrMultiplicationParser: "${operator}"`
+        );
+    }
+  }
+);
+
+export function evaluate(expression: string) {
+  return evaluator(expression, [
+    parenthesisParser,
+    additionOrMultiplicationParser,
+    uselessParenthesisParser,
+  ]);
+}
+
+export function evaluateV2(expression: string) {
+  return evaluator(expression, [
+    parenthesisParser,
+    additionParser,
+    multiplicationParser,
+    uselessParenthesisParser,
+  ]);
+}
+
+type Evaluator = (expression: string) => number;
--- src/18/spec.ts
+++ src/18/spec.ts
@@ -21,7 +21,24 @@ describe("Day 18", () => {
     expect(sumOfAllLines).toEqual(50956598240016);
   });
 
-  it.only("evaluateV2", () => {
+  it("evaluateV2", () => {
+    expect(evaluateV2("1 + 2 + 3")).toEqual(6);
     expect(evaluateV2("1 + 2 * 3 + 4 * 5 + 6")).toEqual(231);
+    expect(evaluateV2("1 + (2 * 3) + (4 * (5 + 6))")).toEqual(51);
+    expect(evaluateV2("2 * 3 + (4 * 5)")).toEqual(46);
+    expect(evaluateV2("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toEqual(1445);
+    expect(evaluateV2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toEqual(
+      669060
+    );
+    expect(
+      evaluateV2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+    ).toEqual(23340);
+  });
+
+  it("Part 2", () => {
+    const sumOfAllLines = input.reduce((sum, line) => {
+      return sum + evaluateV2(line);
+    }, 0);
+    expect(sumOfAllLines).toEqual(535809575344339);
   });
 });
